veruu/patchwork

commit 01b9cbb9ce9f44387be22b5d036dde0fff4bb14b

Include all email headers in mboxes
Solves issue #165 (Exported mboxes should include In-Reply-To,
References, etc headers). Instead of including only a few chosen ones,
all received headers are added to mboxes.

Signed-off-by: Veronika Kabatova <vkabatov@redhat.com>
Reviewed-by: Stephen Finucane <stephen@that.guru>

patchwork/views/utils.py
@@ -89,21 +89,17 @@ def _submission_to_mbox(submission):
     utc_timestamp = delta.seconds + delta.days * 24 * 3600
 
     mail = PatchMbox(body)
-    mail['Subject'] = submission.name
     mail['X-Patchwork-Submitter'] = email.utils.formataddr((
         str(Header(submission.submitter.name, mail.patch_charset)),
         submission.submitter.email))
     mail['X-Patchwork-Id'] = str(submission.id)
     if is_patch and submission.delegate:
         mail['X-Patchwork-Delegate'] = str(submission.delegate.email)
-    mail['Message-Id'] = submission.msgid
     mail.set_unixfrom('From patchwork ' + submission.date.ctime())
 
-    copied_headers = ['To', 'Cc', 'Date', 'From', 'List-Id']
     orig_headers = HeaderParser().parsestr(str(submission.headers))
-    for header in copied_headers:
-        if header in orig_headers:
-            mail[header] = orig_headers[header]
+    for key, val in orig_headers.items():
+        mail[key] = val
 
     if 'Date' not in mail:
         mail['Date'] = email.utils.formatdate(utc_timestamp)

commit 8aa0664fe62b0a7f680c51bd65710dc7060c76d8

Fix stuff around mbox header changes
Bundle tests got broken after the subject in mbox was changed from the
parsed version to the original one because the tests checked for the
presence of patch's name in the response. Fixing this turned out to be
a bit tricky since the tests check the mbox attachment and HTML
responses separately, so we need a string that would be present in both
(the intuitive idea of checking X-Patchwork-Id won't work well).

Add the patch's name to the content of the test patch so we can continue
testing things the same way, checking for the presence of patch's name.

Also add a releasenote notifying about the inclusion of the original
headers.

Reverts: b2a25342 ("Use parsed subject for mboxes")
Fixes: 01b9cbb9 ("Include all email headers in mboxes")
Signed-off-by: Veronika Kabatova <vkabatov@redhat.com>

patchwork/views/utils.py
@@ -101,9 +101,6 @@ def _submission_to_mbox(submission):
     for key, val in orig_headers.items():
         mail[key] = val
 
-    # specifically overwrite the subject with our own nicely formatted name
-    mail['Subject'] = submission.name
-
     if 'Date' not in mail:
         mail['Date'] = email.utils.formatdate(utc_timestamp)
 

releasenotes/notes/mbox-headers-27e1f1bde9570e9f.yaml
@@ -1,0 +1,6 @@
+---
+other:
+  - |
+    mbox files now contain all headers from the original email. This also means
+    the ``Subject: `` header included will contain the original subject and not
+    the parsed Patchwork's version.

patchwork/tests/utils.py
@@ -172,7 +172,7 @@ def create_patch(**kwargs):
         'state': create_state() if 'state' not in kwargs else None,
         'name': 'testpatch%d' % num,
         'headers': '',
-        'content': '',
+        'content': 'Patch testpatch%d' % num,
         'diff': SAMPLE_DIFF,
     }
     values.update(kwargs)